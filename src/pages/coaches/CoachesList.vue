a8f9e71bbc39059803e91059a324ea7cc9b2a45b
André Torquato <andretorquato@alu.ufc.br>
2022-02-08 23:44:32 -0300

    create store

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,18 @@
       <router-link to="/register">Register as coach</router-link>
     </div>
     <ul>
-      LIST OF COACHES
+      <li v-for="coach of coaches" :key="coach.id">{{ coach.firstName }}</li>
     </ul>
   </section>
 </template>	
+
+<script>
+export default {
+  computed: {
+    coaches() {
+      console.log(this.$store.getters['coaches/coaches'])
+      return this.$store.getters['coaches/coaches'];
+    },
+  },
+};
+</script>
