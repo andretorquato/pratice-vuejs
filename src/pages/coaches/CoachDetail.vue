92abb595dddd7ee139588b37abefc96469a1b935
André Torquato <andretorquato@alu.ufc.br>
2022-02-13 23:38:46 -0300

    create coach detail

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,57 @@
 <template>
-	<section>Detail of coach</section>
-	<span><router-link to="/coaches/c1/contact">contact coach</router-link></span>
-	<router-view></router-view>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h2>${{ rate }}</h2>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <h2>Interested? Reach now</h2>
+      <base-button link :to="contactLink">Contact</base-button>
+			<router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+	props: ['id'],
+	data(){
+		return {
+			selectedCoach: null,
+		}
+	},
+	computed: {
+		fullName(){
+			return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+		},
+		contactLink(){
+			return this.$route.path + '/contact';
+		},
+		areas(){
+			return this.selectedCoach.areas;
+		},
+		rate(){
+			return this.selectedCoach.hourlyRate;
+		},
+		description(){
+			return this.selectedCoach.description;
+		},
+	},
+	created() {
+		this.selectedCoach = this.$store.getters['coaches/coaches'].find(coach => coach.id === this.id);
+	}
+}
+</script>
