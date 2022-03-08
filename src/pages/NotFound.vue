72d49de8f8509b0aca0e76b6f7c08e3bc4ad30ae
André Torquato <andretorquato@alu.ufc.br>
2022-03-07 23:29:33 -0300

    not foud

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>404 page not found</h1>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">courses</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
