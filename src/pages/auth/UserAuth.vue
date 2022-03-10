d0db729e1d30041dd7820a42bb22eb4bfc71a00c
André Torquato <andretorquato@alu.ufc.br>
2022-03-09 23:38:27 -0300

    add router auth

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,3 +1,51 @@
 <template>
-	<h1>Login page</h1>
+  <form>
+    <base-card>
+      <div class="form-control">
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div class="form-control">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <div class="form-control">
+        <base-button>Login</base-button>
+        <base-button mode="outline">Signup instead</base-button>
+      </div>
+    </base-card>
+  </form>
 </template>
+
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>	
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
@@ -12,6 +13,7 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
+    { path: '/auth', component: UserAuth },
     {
       path: '/coaches/:id',
       props: true,
