50e4e0ca9753da94a74589e8c1ee877f5faf0961
André Torquato <andretorquato@alu.ufc.br>
2022-03-11 20:09:50 -0300

    userauth interation

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,6 +1,6 @@
 <template>
   <base-card>
-    <form @submit.prevent="signIn">
+    <form @submit.prevent="submitForm">
       <div class="form-control">
         <label for="email">E-mail:</label>
         <input type="email" id="email" v-model.trim="email" required />
@@ -9,9 +9,13 @@
         <label for="password">Password:</label>
         <input type="password" id="password" v-model.trim="password" required />
       </div>
+      <p v-if="!formIsValid">
+        Please enter a valid email and password (must be at least 6 characters
+        long).
+      </p>
       <div class="form-control">
-        <base-button>Login</base-button>
-        <base-button mode="outline">Signup instead</base-button>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button mode="outline" @click="switchModeButton">{{ switchModeButtonCaption }}</base-button>
       </div>
     </form>
   </base-card>
@@ -27,13 +31,34 @@ export default {
       mode: 'login',
     };
   },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode == 'login') return 'Login';
+      else return 'Signup';
+    },
+    switchModeButtonCaption() {
+      if (this.mode == 'login') return 'Signup instead';
+      else return 'Login instead';
+    },
+  },
   methods: {
-    signIn(){
-      if(!this.email || this.email.includes('@')){
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        (this.email && !this.email.includes('@')) ||
+        this.password.length < 6
+      ) {
         this.formIsValid = false;
         return;
       }
-    }
+      // request
+    },
+    switchModeButton() {
+      if (this.mode == 'login') 
+        this.mode = 'signup';
+      else 
+        this.mode = 'login';
+    },
   },
 };
 </script>
