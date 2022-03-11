ce44200c5f559f45b9244bed3b55fae8edfc545d
André Torquato <andretorquato@alu.ufc.br>
2022-03-10 23:52:29 -0300

    email validation

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,23 +1,42 @@
 <template>
-  <form>
-    <base-card>
+  <base-card>
+    <form @submit.prevent="signIn">
       <div class="form-control">
         <label for="email">E-mail:</label>
-        <input type="email" id="email" v-model="email" required />
+        <input type="email" id="email" v-model.trim="email" required />
       </div>
       <div class="form-control">
         <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required />
+        <input type="password" id="password" v-model.trim="password" required />
       </div>
       <div class="form-control">
         <base-button>Login</base-button>
         <base-button mode="outline">Signup instead</base-button>
       </div>
-    </base-card>
-  </form>
+    </form>
+  </base-card>
 </template>
 
-
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      formIsValid: true,
+      mode: 'login',
+    };
+  },
+  methods: {
+    signIn(){
+      if(!this.email || this.email.includes('@')){
+        this.formIsValid = false;
+        return;
+      }
+    }
+  },
+};
+</script>
 <style scoped>
 form {
   margin: 1rem;
