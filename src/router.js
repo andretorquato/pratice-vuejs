62ba15ee56ed9964738700337c635e1608eb123d
André Torquato <andretorquato@alu.ufc.br>
2022-03-22 22:32:11 -0300

    add validation to access routes

--- src/router.js
+++ src/router.js
@@ -7,23 +7,33 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: CoachesList },
-    { path: '/auth', component: UserAuth },
+    { path: '/coaches', component: CoachesList},
+    { path: '/auth', component: UserAuth, meta: { requiredUnAuth: true }},
     {
       path: '/coaches/:id',
       props: true,
       component: CoachDetail,
       children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestsReceived },
+    { path: '/register', component: CoachRegistration, meta: { requiredAuth: true }  },
+    { path: '/requests', component: RequestsReceived, meta: { requiredAuth: true }},
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
+router.beforeEach((to, _, next)=> {
+  if(to.meta.requiredAuth && !store.getters.isAuthenticated){
+    next('/auth');
+  } else if(to.meta.requiredUnAuth && store.getters.isAuthenticated){
+    next('/coaches');
+  } else {
+    next();
+  }
+});
 export default router;
