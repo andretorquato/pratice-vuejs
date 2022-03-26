7e392e75dbee742b48aed77d35377bf87df45a81
André Torquato <andretorquato@alu.ufc.br>
2022-03-25 23:27:19 -0300

    auto logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,5 @@
+let timer;
+
 export default {
   async auth(context, payload){
     const query = payload.query;
@@ -15,15 +17,25 @@ export default {
       const error = new Error(data.message || errorMessage);
       throw error;
     } 
+    
+    const expirationIn = +data.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expirationIn;
+    
+    setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expirationIn);
+
     localStorage.setItem('token', data.idToken);
     localStorage.setItem('userId', data.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
     context.commit('setUser', {
       token: data.idToken,
-      userId: data.localId,
-      tokenExpiration: data.expiresIn,
+      userId: data.localId
     });
   },
   login(context, payload) {
+    clearTimeout(timer);
     context.dispatch('auth', {
       ...payload,
       query: 'signInWithPassword',
@@ -38,11 +50,25 @@ export default {
     });
   },
   logout(context){
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
     context.commit('logout');
   },
   tryLogin(context){
     const token = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+    
+    const expireIn = +tokenExpiration - new Date().getTime();
+    if(expireIn < 0){
+      context.dispatch('logout');
+      return;
+    }
+
+    setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expireIn);
     if(!token || !userId){
       return;
     }
@@ -50,5 +76,9 @@ export default {
       token,
       userId,
     });
+  },
+  autoLogout(context){
+    context.commit('logout');
+    context.commit('autoLogout');
   }
 };
--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -2,11 +2,13 @@ export default {
   setUser(state, payload) {
     state.userId = payload.userId;
     state.token = payload.token;
-    state.tokenExpiration = payload.tokenExpiration;
   },
   logout(state) {
     state.userId = null;
     state.token = null;
-    state.tokenExpiration = null;
+    state.didAutoLogout = false;
   },
+  autoLogout(state) {
+    state.didAutoLogout = true;
+  }
 };
